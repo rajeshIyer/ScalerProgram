b9126e560f27e980cb9cbf5f9c8028ca005f0ab8
rajesh_iyer <rajesh.iyer0723@gmail.com>
2023-01-17 01:42:41 -0500

    initial commit

--- src/IntroProbSolving/IntroProbSolving/Lecture/Sum_Of_N_Natural_Nos.java
+++ src/IntroProbSolving/IntroProbSolving/Lecture/Sum_Of_N_Natural_Nos.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class Sum_Of_N_Natural_Nos {
 
+    public long sumOfNNaturalNos(int n) {
+        return ((long)(n*(n+1))/2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Enter the natural number n; for which we wish to find the sum:");
+        System.out.println("To find the sum of n natural numbers; enter n: ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        long sum = ((long)(n*(n+1))/2);
+
+        Sum_Of_N_Natural_Nos sumOfNNaturalNos = new Sum_Of_N_Natural_Nos();
+        long sum = sumOfNNaturalNos.sumOfNNaturalNos(n);
         System.out.println("The sum of first N=" + n  + " natural numbers is Sum=" + sum + ".");
 
         // Instead of adding elements one by one that would lead to TC of O(N), we instead use the formula.
