4669f7fd376f6cfed38cecb3ccdd21ea74b3d135
rajesh_iyer <rajesh.iyer0723@gmail.com>
2023-01-17 02:47:28 -0500

    initial commit

--- src/IntroProbSolving/IntroProbSolving/Lecture/DivideByTwoToReachOne.java
+++ src/IntroProbSolving/IntroProbSolving/Lecture/DivideByTwoToReachOne.java
@@ -20,4 +20,12 @@ public class DivideByTwoToReachOne {
         int count = divideByTwoToReachOne.fetchCountOfDivisionsByTwoToReachOne(n);
         System.out.println("The number of divisions by 2 needed to reach 1 for the number=" + n + " is " + "count=" + count);
     }
+
+    //If N = 2^k, k is the answer.
+    //For all other N , find the closest power of 2 less than N as the answer
+    //https://www.khanacademy.org/math/algebra2/x2ec2f6f830c9fb89:logs/x2ec2f6f830c9fb89:log-intro/a/intro-to-logarithms
+    //
+    //(logN) base 2: Number of times N has to be divided by 2 to make it 1.
+    // TC = O(logN base 2)
+    // SC = O(1)
 }
